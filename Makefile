64b73fdabf3c86e71b91ddf24521fb11866912d3
supernukuomu <kpa6ac@gmail.com>
2024-08-01 19:07:13 +0300

    init

---
+++ Makefile
@@ -0,0 +1,7 @@
+install:
+	npm ci
+lint:
+	npx eslint .
+
+publish:
+	npm publish --dry-run
---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference');
+
+program.parse();
