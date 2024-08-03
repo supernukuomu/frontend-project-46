24b7b536ef0d15224f51565774da89305bc3780c
supernukuomu <kpa6ac@gmail.com>
2024-08-03 18:47:59 +0300

    init

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,10 @@ import { program } from 'commander';
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference');
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information');
 
 program.parse();
