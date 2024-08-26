cb7f69c99eba5a926bad5d6ca669b1e8f0a66984
supernukuomu <kpa6ac@gmail.com>
2024-08-26 20:23:38 +0300

    modified

--- Makefile
+++ Makefile
@@ -7,6 +7,12 @@ gendiff:
 gendiff-start:
 	node bin/gendiff.js __fixtures__/file1.json __fixtures__/file2.json
 
+test:
+	npm test
+
+test-coverage:
+	npm test -- --coverage --coverageProvider=v8
+
 lint:
 	npx eslint .
 
---
+++ __tests__/index.test.js
@@ -0,0 +1,18 @@
+import getDifference from '../src/index.js';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) =>
+  join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('getDifference', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  expect(getDifference(filepath1, filepath2)).toEqual(
+    readFile('expectedDiff.txt')
+  );
+});
